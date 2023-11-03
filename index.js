485bef3a4d0e21af481373be29d01fe9538f0e2b
Ip <exestman@gmail.com>
2023-11-03 14:26:58 +0300

    -v 1.1.3

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api'),
     fs = require("fs"),
     { Key, telegramSettings, PathData} = require("./lib/config"),
-    SecuritySystem = require('./lib/security')
-const {getMotivation} = require("./lib/extra");
-DataHandler = require("./lib/datahander"),
+    SecuritySystem = require('./lib/security'),
+    DataHandler = require("./lib/datahander"),
     Admin = require('./lib/admin')
-client = new TelegramBot(Key, {
-    polling: true,
+    client = new TelegramBot(Key, {
+        polling: true,
 })
 
 global.XMLHttpRequest = require('xhr2');
@@ -18,17 +17,17 @@ client.on('message', async function (msg) {
     SecuritySystem.isAdmin(msg.from.username, async function (result) { /** works */
         if (result === true)
             try{
-                await client.sendMessage(msg.chat.id,`${msg.video.file_id} ID видео, для вставки в поле с видео`);
+                await client.sendMessage(msg.chat.id,` \`${msg.video.file_id}\` ID видео, для вставки в поле с видео`);
             }catch (e) {}
         try{
             let filepath= `./lib/storage/files/${msg.document.file_name}`;
             fs.writeFile(filepath, '-',{}, function () {})
-            let fileWriter= fs.createWriteStream(filepath); //creating stream for writing to file
+            let fileWriter= fs.createWriteStream(filepath);
             const getReadStreamPromise = () => {
                 return new Promise((resolve, reject) => {
-                    const stream = client.getFileStream(msg.document.file_id); //getting stream to file bytes
+                    const stream = client.getFileStream(msg.document.file_id);
                     stream.on('data', (chunk) => {
-                        fileWriter.write(chunk); //copying to our file chunk by chunk
+                        fileWriter.write(chunk);
                     })
                     stream.on('error', (err) => {
                         reject(err);
@@ -58,49 +57,50 @@ client.on('message', async function (msg) {
                 }
             }, 2000)
         } else {
-            for (const u of JSON.parse(SecuritySystem.getDataAuth_user())) {
-                if (msg.from.username === u) { /** works */
-                    await DataHandler.logger(
-                        msg.from,
-                        msg.from.username,
-                        msg.text,
-                        !0
-                    )
-                    console.log(true)
-                    if (msg.text === '/logrender') {
-                        await client.sendDocument(msg.chat.id.toString(), DataHandler.getRenderLog(msg.from.username, msg.from.id, msg.message_id, getDate()))
-                    }
-                    else if (msg.text === '/getbanlist') {
-                        /** works */
-                        SecuritySystem.isAdmin(msg.from.username, async function (result) {
-                            if (result === true)
-                                try {
-                                    await client.sendMessage(msg.chat.id, Admin._getbanlist().content.toString())
-                                } catch (e) {
+            try {
+                JSON.parse(SecuritySystem.getDataAuth_user())
+                for (const u of JSON.parse(SecuritySystem.getDataAuth_user())) {
+                    if (msg.from.username === u) { /** works */
+                        await DataHandler.logger(
+                            msg.from,
+                            msg.from.username,
+                            msg.text,
+                            !0
+                        )
+                        console.log(true)
+                        if (msg.text === '/logrender') {
+                            await client.sendDocument(msg.chat.id.toString(), DataHandler.getRenderLog(msg.from.username, msg.from.id, msg.message_id, getDate()))
+                        }
+                        else if (msg.text === '/getbanlist') {
+                            /** works */
+                            SecuritySystem.isAdmin(msg.from.username, async function (result) {
+                                if (result === true)
                                     try {
-                                        await client.sendDocument(msg.chat.id, Admin._getbanlist().path)
+                                        await client.sendMessage(msg.chat.id, Admin._getbanlist().content.toString())
                                     } catch (e) {
-                                        await client.sendMessage(msg.chat.id, 'Произошла ошибка при получении файла')
+                                        try {
+                                            await client.sendDocument(msg.chat.id, Admin._getbanlist().path)
+                                        } catch (e) {
+                                            await client.sendMessage(msg.chat.id, 'Произошла ошибка при получении файла')
+                                        }
                                     }
-                                }
-                        })
-                    }
-                    else if (msg.text === '/getmemberslist') {
-                        /** works */
-                        SecuritySystem.isAdmin(msg.from.username, async function (result) {
-                            if (result === true)
-                                try {
-                                    await client.sendMessage(msg.chat.id, Admin._getmemberslist().content.toString())
-                                } catch (e) {
+                            })
+                        }
+                        else if (msg.text === '/getmemberslist') {
+                            SecuritySystem.isAdmin(msg.from.username, async function (result) {
+                                if (result === true)
                                     try {
-                                        await client.sendDocument(msg.chat.id, Admin._getmemberslist().path)
+                                        await client.sendMessage(msg.chat.id, Admin._getmemberslist().content.toString())
                                     } catch (e) {
-                                        await client.sendMessage(msg.chat.id, 'Произошла ошибка при получении файла')
+                                        try {
+                                            await client.sendDocument(msg.chat.id, Admin._getmemberslist().path)
+                                        } catch (e) {
+                                            await client.sendMessage(msg.chat.id, 'Произошла ошибка при получении файла')
+                                        }
                                     }
-                                }
-                        })
-                    }
-                    else if (msg.text === '/getlog') { /** works */
+                            })
+                        }
+                        else if (msg.text === '/getlog') { /** works */
                         SecuritySystem.isAdmin(msg.from.username, async function (result) {
                             if (result === true)
                                 try {
@@ -110,22 +110,22 @@ client.on('message', async function (msg) {
                                     await client.sendMessage(msg.chat.id, 'Произошла ошибка при получении файла')
                                 }
                         })
-                    }
-                    else if (msg.text === '/getadminslist') { /** works */
-                    SecuritySystem.isAdmin(msg.from.username, async function (result) {
-                        if (result === true)
-                            try {
-                                await client.sendMessage(msg.chat.id, Admin._getadminlist().content.toString())
-                            } catch (e) {
+                        }
+                        else if (msg.text === '/getadminslist') { /** works */
+                        SecuritySystem.isAdmin(msg.from.username, async function (result) {
+                            if (result === true)
                                 try {
-                                    await client.sendDocument(msg.chat.id, Admin._getadminlist().path)
+                                    await client.sendMessage(msg.chat.id, Admin._getadminlist().content.toString())
                                 } catch (e) {
-                                    await client.sendMessage(msg.chat.id, 'Произошла ошибка при получении файла')
+                                    try {
+                                        await client.sendDocument(msg.chat.id, Admin._getadminlist().path)
+                                    } catch (e) {
+                                        await client.sendMessage(msg.chat.id, 'Произошла ошибка при получении файла')
+                                    }
                                 }
-                            }
-                    })
-                    }
-                    else if (msg.text === '/getfileslist') { /** works */
+                        })
+                        }
+                        else if (msg.text === '/getfileslist') { /** works */
                         SecuritySystem.isAdmin(msg.from.username, async function (result) {
                             if (result === true)
                                 try {
@@ -134,49 +134,55 @@ client.on('message', async function (msg) {
                                     await client.sendMessage(msg.chat.id, 'Произошла ошибка при получении данных')
                                 }
                         })
-                    }
-                    else if (msg.text === '/help') { /** works */
+                        }
+                        else if (msg.text === '/help') { /** works */
                         await client.sendMessage(msg.chat.id, Admin._help())
-                    }
-                    else if (msg.text === '/cls') { /** works */
+                        }
+                        else if (msg.text === '/cls') { /** works */
                         SecuritySystem.isAdmin(msg.from.username, function (result) {
                             if (result === true)
                                 console.clear();
                         })
-                    }
-                    else if (msg.text === '/ref') { /** works */
-                        SecuritySystem.isAdmin(msg.from.username, function (result) {
-                            if (result === true)
-                                Admin._refresh_data();
-                        })
-                    }
-                    else await DataHandler.responseRenderer(msg.text, async function (d) {
-                        if (isKeyBoard(await d)) {
-                            client.sendMessage(msg.chat.id, msg.text, await d)
-                                .then(() => client.deleteMessage(msg.chat.id, msg.message_id))
-                                .then(() => client.deleteMessage(msg.from.id, msg.message_id))
-                        } else {
-                            client.sendMessage(msg.chat.id, await d, {
-                                parse_mode: telegramSettings.parse_mode
+                        }
+                        else if (msg.text === '/ref') { /** works */
+                            await SecuritySystem.isAdmin(msg.from.username, async function (result) {
+                                if (result === true)
+                                    await Admin._refresh_data();
                             })
-                            .then(() => DataHandler.callFileFromRenderer(msg.text, function (res) {
-                                try {
-                                    client.sendDocument(msg.chat.id, res)
-                                } catch (e) {
-                                }
-                            }))
-                            try {
-                                await DataHandler.callMediaFromRenderer(msg.text, async function (res) {
+                        }
+                        else await DataHandler.responseRenderer(msg.text, async function (d) {
+                            if (isKeyBoard(await d)) {
+                                client.sendMessage(msg.chat.id, msg.text, await d)
+                                    .then(() => client.deleteMessage(msg.chat.id, msg.message_id))
+                                    .then(() => client.deleteMessage(msg.from.id, msg.message_id))
+                            } else {
+                                client.sendMessage(msg.chat.id, await d, {
+                                    parse_mode: telegramSettings.parse_mode
+                                }).then(() => DataHandler.callFileFromRenderer(msg.text, function (res) {
                                     try {
-                                        await client.sendVideo(msg.chat.id, res);
+                                        client.sendDocument(msg.chat.id, res)
                                     } catch (e) {
                                     }
-                                })
-                            } catch (e) {
+                                }))
+                                try {
+                                    await DataHandler.callMediaFromRenderer(msg.text, async function (res) {// TODO: Фикс видео
+                                        try{
+                                            await client.sendVideo(msg.chat.id, res, {
+                                                reply_to_message_id: msg.from.message_id
+                                            });
+                                        }catch (e) {
+
+                                        }
+                                    })
+                                } catch (e) {
+                                }
                             }
-                        }
-                    })
+                        })
+                        break;
+                    }
                 }
+            }catch (e) {
+                console.log('to many requests')
             }
         }
     })
